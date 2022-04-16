9c82f09caa96602f3ae6613fab4ade80d1bb8c1b
needm0resleep <kristinedelacruz.uson@gmail.com>
2022-04-16 14:09:04 +0200

    API work - POST and DELETE

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.listen(port, () => {
 });
 
 // FireBase Admin SDK configuration
-
 const { initializeApp } = require("firebase-admin/app");
 
 const admin = require("firebase-admin");
@@ -19,7 +18,7 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-// API
+// API Methods
 
 const hamsters = db.collection("data");
 
@@ -27,8 +26,8 @@ app.get("/", (req, res) => {
   res.send("hi");
 });
 
+// Get ALL Hamsters
 app.get("/hamsters", async (req, res) => {
-  // Get ALL Hamsters
   try {
     const snapshot = await hamsters.get();
     const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
@@ -38,24 +37,52 @@ app.get("/hamsters", async (req, res) => {
   }
 });
 
+// GET a RANDOM Hamster
 app.get("/hamsters/random", async (req, res) => {
-  // Get a RANDOM Hamster
   const snapshot = await hamsters.get();
   const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   res.send(list[Math.floor(Math.random() * 41)]);
 });
 
+// GET a SPECIFIC Hamster
 app.get("/hamsters/:id", async (req, res) => {
-  // Get a SPECIFIC Hamster
-
   try {
     const docRef = db.collection("data").doc(req.params.id);
     const doc = await docRef.get();
     if (!doc.exists) {
-      res.status(404).send("ERROR: 404");
+      res.status(404).send("ERROR: Hamster does not exist :'(");
     } else {
       res.send(doc.data());
     }
+  } catch (err) {
+    res.status(500).send("ERROR: " + err);
+  }
+});
+
+// Add(POST) a new hamster
+app.post("/hamsters", (req, res) => {
+  try {
+    const res = hamsters.add({
+      games: 0,
+      wins: 0,
+      favFood: "Pizza",
+      imgName: 0,
+      age: 2,
+      defeats: 0,
+      name: "Alex",
+      loves: "Fighting",
+    });
+    res.send("New hamster added");
+  } catch (err) {
+    res.status(500).send("ERROR: " + err);
+  }
+});
+
+// DELETE a specific hamster
+app.delete("/hamsters/:id", (req, res) => {
+  try {
+    db.collection("data").doc(req.params.id).delete();
+    res.send("Hamster is removed");
   } catch (err) {
     res.status(500).send("ERROR: " + err);
   }
