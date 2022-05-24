27ecd33e9bd61c068d7273c482090778da6a8744
needm0resleep <kristinedelacruz.uson@gmail.com>
2022-05-24 21:25:59 +0200

    Revert "React App"
    This reverts commit d181f78e3ffa95eebc83c6f7fdab9a9edf92d60f.

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,6 @@ const db = admin.firestore();
 // API Methods
 
 const hamsters = db.collection("data");
-const matches = db.collection("matches");
 
 app.get("/", (req, res) => {
   res.send("hi");
@@ -43,7 +42,7 @@ app.get("/hamsters", async (req, res) => {
 app.get("/hamsters/random", async (req, res) => {
   const snapshot = await hamsters.get();
   const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  res.send(list[Math.floor(Math.random() * list.length)]);
+  res.send(list[Math.floor(Math.random() * 41)]);
 });
 
 // GET a SPECIFIC Hamster
@@ -69,11 +68,11 @@ app.post("/hamsters", async (req, res) => {
     favFood: "Pizza",
     imgName: 0,
     age: 2,
-    defeats: 1,
-    name: "rebecca",
+    defeats: 0,
+    name: "Alex",
     loves: "Fighting",
   });
-  res.status(200).send({ id: docRef.id });
+  res.send("New hamster is created! The new ID is " + docRef.id);
 });
 
 // EDIT a specific hamster
@@ -104,134 +103,3 @@ app.delete("/hamsters/:id", (req, res) => {
     res.status(500).send("ERROR: " + err);
   }
 });
-
-// Get ALL the matches
-app.get("/matches", async (req, res) => {
-  try {
-    const snapshot = await matches.get();
-    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.send(list);
-  } catch (err) {
-    res.status(500);
-  }
-});
-
-// GET a SPECIFIC match
-app.get("/matches/:id", async (req, res) => {
-  try {
-    const docRef = db.collection("matches").doc(req.params.id);
-    const doc = await docRef.get();
-    if (!doc.exists) {
-      res.sendStatus(404);
-    } else {
-      res.send(doc.data());
-    }
-  } catch (err) {
-    res.status(500).send("ERROR: " + err);
-  }
-});
-
-// Add(POST) a new match
-app.post("/matches", async (req, res) => {
-  const docRef = await matches.add({
-    winnerId: "Z472QJsViABky4H2DfoV",
-    loserId: "8f0A2yORPPvGCFH1y9RN",
-  });
-  res.status(200).send({ id: docRef.id });
-});
-
-// DELETE a specific match
-app.delete("/matches/:id", (req, res) => {
-  try {
-    db.collection("matches").doc(req.params.id).delete();
-    res.sendStatus(200);
-  } catch (err) {
-    res.status(500).send("ERROR: " + err);
-  }
-});
-
-// GET matches who has a specific hamster as winner
-app.get("/matchWinners/:id", async (req, res) => {
-  db.collection("matches")
-    .where("winnerId", "==", req.params.id)
-    .get()
-    .then((matchDocs) => {
-      const list = matchDocs.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      if (list.length === 0) {
-        res.sendStatus(404);
-      } else {
-        res.send(list);
-      }
-    });
-});
-
-// GET hamsters with highest wins
-app.get("/winners", async (req, res) => {
-  db.collection("data")
-    .where("wins", ">", 0)
-    .orderBy("wins")
-    .limit(5)
-    .get()
-    .then((matchDocs) => {
-      const list = matchDocs.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      res.send(list);
-    });
-});
-
-// GET hamsters with highest defeats
-app.get("/losers", async (req, res) => {
-  db.collection("data")
-    .where("defeats", ">", 0)
-    .orderBy("defeats")
-    .limit(5)
-    .get()
-    .then((matchDocs) => {
-      const list = matchDocs.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      res.send(list);
-    });
-});
-
-// Cutest Hamster WIP
-/*app.get("/hamsters/cutest", async (req, res) => {
-  try {
-    const snapshot = await hamsters.get();
-    const list = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-      thing: doc.data().age,
-    }));
-    res.send(list.orderBy("result").limit(5));
-  } catch (err) {
-    res.status(500);
-  }
-});*/
-
-/*app.get("/hamsters/cutest", async (req, res) => {
-  db.collection("data")
-    .where("wins", ">", 0)
-    .orderBy("wins")
-    .limit(5)
-    .get()
-    .then((matchDocs) => {
-      const list = matchDocs.docs.map((doc) => ({
-        id: doc.id,
-        result: doc.wins - doc.defeats,
-        ...doc.data(),
-      }));
-      list.where("result", ">", 0).orderBy("result");
-      const cutest = list.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      res.send(cutest);
-    });
-});*/
