eb47c5c9c6daf1577f08e2ce402660bdb94c7cad
needm0resleep <kristinedelacruz.uson@gmail.com>
2022-05-24 22:29:08 +0200

    heroku update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Express - Minimal server
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 1337; // funkar på Heroku
+const port = process.env.PORT || 1337;
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
