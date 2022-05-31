f9248164d792c7eb22bddce2a4bf797d7cf3e0ae
needm0resleep <kristinedelacruz.uson@gmail.com>
2022-05-31 17:36:24 +0200

    port

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static("frontend"));
 app.use(express.json());
